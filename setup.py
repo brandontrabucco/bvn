50e8a19839b86baa5b83a58af3325bc2aa2eb6fc
Brandon <brandon@btrabucco.com>
2020-09-20 16:59:44 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from setuptools import find_packages
 from setuptools import setup
 
 
-PACKAGES = ['tensorflow', 'numpy', 'networkx']
-
-
-setup(name='bvn',
-      description='A Greedy Birkhoff-Von neumann Decomposition',
-      license='MIT',
-      version='0.1',
-      zip_safe=True,
-      include_package_data=True,
-      packages=find_packages(),
-      install_requires=PACKAGES)
+setup(
+  name='bvn',
+  packages=['bvn'],
+  version='0.1',
+  license='MIT',
+  description='A Greedy Birkhoff-Von Neumann Decomposition',
+  author='Brandon Trabucco',
+  author_email='brandon@btrabucco.com',
+  url='https://github.com/brandontrabucco/bvn',
+  download_url='https://github.com/brandontrabucco/bvn/archive/v_01.tar.gz',
+  keywords=['Permutation', 'Doubly Stochastic', 'Birkhoff-von Neumann', 'Decomposition'],
+  install_requires=['networkx', 'numpy', 'tensorflow'],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Scientists',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
